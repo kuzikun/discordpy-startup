048f33fa41a349f268cee09d8dd069c38550090c
kuzikun <68470540+kuzikun@users.noreply.github.com>
2020-07-19 19:51:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,6 +11,13 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
+
+import random 
+   if message.content == "樋□さんお話しして！":
+　　　　　#↓
+       hennnahuri = ["せんわ", "みんなでハワイ行きたいなぁ", "整地してるから黙って？", "あ", "そのフリはつまらん", "君はもう少し服にお金かけてもいいと思うよ", "無理"]
+       choice = random.choice(hennnahuri)＃←
+       await message.channel.send(choice)
    
 @bot.command()
 async def ping(ctx):
